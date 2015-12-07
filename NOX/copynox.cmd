d9d36f43a47ba493e85ba2b30eb5054bf5294db5
corp\swhittier <swhittier@maxex.com>
2015-12-07 08:32:20 -0500

    Updated to use junction c:\tools\bin\MAX

--- NOX/copynox.cmd
+++ NOX/copynox.cmd
@@ -1,4 +1,4 @@
 REM Copy CMD Files...
-ECHO F|xcopy /S /F /R /Y buildnox4dev.cmd C:\Tools\Bin\buildnox4dev.cmd
-ECHO F|xcopy /S /F /R /Y buildnoxlocal.cmd C:\Tools\Bin\buildnoxlocal.cmd
-ECHO F|xcopy /S /F /R /Y buildnoxremote.cmd C:\Tools\Bin\buildnoxremote.cmd
+ECHO F|xcopy /S /F /R /Y buildnox4dev.cmd C:\Tools\Bin\MAX\buildnox4dev.cmd
+ECHO F|xcopy /S /F /R /Y buildnoxlocal.cmd C:\Tools\Bin\MAX\buildnoxlocal.cmd
+ECHO F|xcopy /S /F /R /Y buildnoxremote.cmd C:\Tools\Bin\MAX\buildnoxremote.cmd

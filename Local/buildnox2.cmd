382f419a09974b090fe98a6863ba6d76c615e6c5
meansteve74 <meansteve74@gmail.com>
2015-12-10 13:08:45 -0500

    Updated to read NOXHOST from environment variable.

--- Local/buildnox2.cmd
+++ Local/buildnox2.cmd
@@ -1,16 +1,20 @@
 echo off
 
+REM In order to run this script, you must set up the following environment variables:
+REM NOXHOST = The machine name of your NOX Server
+REM NOXBUILDUSER = The domain user account name you use to log into your NOX Server
+REM NOXBUILDPASSWORD = The domain user account password you use to log into your NOX Server.
+
 REM Initialization
-set __NOXHOST=STVNOX
 set __NOXBUILDTOOLSDIR=c:\tools\bin\MAX
 set __GITREPODIR=C:\Wipro Gallagher Solutions\NetOxygen
 
 echo Launching remote NOX build on %__NOXHOST%...
 
-psexec \\%__NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git pull --verbose
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git pull --verbose
 IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
 
-psexec \\%__NOXHOST% -w %__NOXBUILDTOOLSDIR% buildnoxremote.cmd
+psexec \\%NOXHOST% -w %__NOXBUILDTOOLSDIR% buildnoxremote.cmd
 IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
 
 goto Fin

8d39d248f9b36b84e8f5e8ea15c4388bdbea1de4
meansteve74 <meansteve74@gmail.com>
2015-12-11 09:25:15 -0500

    Removed quotes from Echo messages.

--- NOX/buildnox4dev.cmd
+++ NOX/buildnox4dev.cmd
@@ -1,11 +1,11 @@
 REM Builds NOX locally without fetch source from TFS.  For use in development.
 
-echo "Building interpreter scripts"
+echo Building interpreter scripts
 DotNetScriptCompiler.exe /Rebuild
 pskill GFXServer.exe
 iisreset
 
-echo "Building MAX Portal Services..."
+echo Building MAX Portal Services...
 msbuild "C:\Workspaces\MAX\Code\Dev\Net Oxygen\Server\Services\MaxPortalServices\MaxPortalServices.csproj" /p:VisualStudioVersion=12.0
 
-echo "NOX build complete"
+echo NOX build complete
--- NOX/buildnoxlocal.cmd
+++ NOX/buildnoxlocal.cmd
@@ -1,16 +1,16 @@
 REM Get's the latest source from TFS and builds NOX locally.
 
-echo "Getting latest from TFS..."
+echo Getting latest from TFS...
 
 cd "C:\Wipro Gallagher Solutions\NetOxygen"
 git pull --verbose
 
-echo "Building interpreter scripts"
+echo Building interpreter scripts
 DotNetScriptCompiler.exe /Rebuild
 pskill GFXServer.exe
 iisreset
 
-echo "Building MAX Portal Services..."
+echo Building MAX Portal Services...
 msbuild "C:\Workspaces\MAX\Code\Dev\Net Oxygen\Server\Services\MaxPortalServices\MaxPortalServices.csproj" /p:VisualStudioVersion=12.0
 
-echo "NOX build complete"
+echo NOX build complete

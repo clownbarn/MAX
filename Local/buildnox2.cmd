2abd5d2abb97354fcb3155d0d5ed7f127951bcd6
meansteve74 <meansteve74@gmail.com>
2015-12-21 11:09:56 -0500

    Check out the project files that get altered by the NOX script compiler prior to pulling.

--- Local/buildnox2.cmd
+++ Local/buildnox2.cmd
@@ -11,6 +11,41 @@ set __GITREPODIR=C:\Wipro Gallagher Solutions\NetOxygen
 
 echo Launching remote NOX build on %__NOXHOST%...
 
+REM Refresh Project files altered by the NOX .NET Script Compiler.
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Autotasks\Script.AutoTasks.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Controls\Script.Controls.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Defaults\Script.Defaults.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Dialogs\Script.Dialogs.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Dialogs\Script.Dialogs.VB.vbproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Documents\Script.Documents.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Fees\Script.Fees.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\FieldValidation\Script.FieldValidation.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\HotFieldInfo\Script.HotFieldInfo.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Interfaces\Script.Interfaces.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\PaymentStreams\Script.PaymentStreams.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Services\Script.Services.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Shared\Script.Shared.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Shared\Script.Shared.VB.vbproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\Workflow\Script.Workflow.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git checkout -- scripts\WorkflowCustom\Script.WorkflowCustom.C#.csproj
+IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
+
+REM Get Latest Source.
 psexec \\%NOXHOST% -w "%__GITREPODIR%" -u %NOXBUILDUSER% -p %NOXBUILDPASSWORD% git pull --verbose
 IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
 

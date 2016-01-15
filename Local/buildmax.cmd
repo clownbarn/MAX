3aad7ea849a6f844891d962bcf509cabc587ab9b
meansteve74 <meansteve74@gmail.com>
2016-01-15 10:48:39 -0500

    Perform database publish before building portal solution. Removed deprecated variables for database projects that have been removed from portal solution.

--- Local/buildmax.cmd
+++ Local/buildmax.cmd
@@ -5,15 +5,8 @@ set __MAXBUILDFAILED=0
 set __BUILDTOOLSDIR=C:\tools\bin\MAX
 set __USERBUILDTOOLSDIR=%USERPROFILE%\tools\bin
 set __WORKSPACEDIR=C:\Workspaces\Code\Dev
-set __DOCGENSOLUTIONPATH="%__WORKSPACEDIR%\Application Layer\Max.Business.DocumentGenerator\Max.Business.DocumentGenerator.sln"
-set __DOCGENOUTPUTPATH="%__WORKSPACEDIR%\Application Layer\Max.Business.DocumentGenerator\bin\Debug\Max.Business.DocumentGenerator.exe"
 set __MAXSOLUTIONPATH=%__WORKSPACEDIR%\Solutions\Max.sln
 set __MAXPORTALOUTPUTPATH="%__WORKSPACEDIR%\Web Applications\Max.UI.Portal\bin\Max.UI.Portal.dll"
-set __PRICINGDATAPROJECTPATH=%__WORKSPACEDIR%\PricingService\Max.Database.PricingService\Max.Database.PricingService.sqlproj
-set __PRICINGDATAOUTPUTPATH=%__WORKSPACEDIR%\PricingService\Max.Database.PricingService\bin\Output\Max.Database.PricingService.dacpac
-set __MAXDATAPROJECTPATH=%__WORKSPACEDIR%\Data\Max.Database\Max.Database.Schema.sqlproj
-set __MAXDATAOUTPUTPATH=%__WORKSPACEDIR%\Data\Max.Database\bin\Output\Max.Database.Schema.dacpac
-
 
 REM Reset IIS
 echo Resetting IIS...
@@ -32,19 +25,19 @@ IF NOT '%ERRORLEVEL%' =='0' (
 )
 IF %__MAXBUILDFAILED% == 1 (goto BuildFailed)
 
-REM Build Max Portal solution 
-echo Building Max Portal Solution...
-devenv %__MAXSOLUTIONPATH% /clean
-devenv %__MAXSOLUTIONPATH% /build debug
-IF NOT EXIST %__MAXPORTALOUTPUTPATH% (goto BuildFailed)
-echo Max Portal Solution Build complete.
-
 REM Publish databases
 echo Publishing databases...
 REM call %__BUILDTOOLSDIR%\publishdbs2.cmd
 call %__BUILDTOOLSDIR%\publishdbs2.cmd
 IF NOT '%ERRORLEVEL%' =='0' (goto BuildFailed)
 echo Database publishing complete.
+
+REM Build Max Portal solution 
+echo Building Max Portal Solution...
+devenv %__MAXSOLUTIONPATH% /clean
+devenv %__MAXSOLUTIONPATH% /build debug
+IF NOT EXIST %__MAXPORTALOUTPUTPATH% (goto BuildFailed)
+echo Max Portal Solution Build complete.
 
 REM Build NOX interpreter scripts and MaxPortalServices
 echo Performing NOX build step...
